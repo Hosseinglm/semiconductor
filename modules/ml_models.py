5ca6782a8bfc9c765dbc06f78f2c1844eed8776d
Hosseinglm <Hossein.glmm@gmail.com>
2024-12-17 09:06:22 +1100

    Ensure y is integer type for classification

--- modules/ml_models.py
+++ modules/ml_models.py
@@ -12,7 +12,8 @@ class AnomalyDetector:
 
     def train_supervised(self, X, y):
         """Train supervised model for defect prediction."""
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        y = y.astype(int)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=101)
         self.random_forest.fit(X_train, y_train)
         return self.random_forest.score(X_test, y_test)
 
